f2c98b7adf442e542029703c9a5c9cdf0172d861
KingBlob <joshapennington20@gmail.com>
2025-05-03 22:11:07 -0700

    Found issue: webpage overwriting

--- SpringCrawler/spiders/url.py
+++ SpringCrawler/spiders/url.py
@@ -6,7 +6,7 @@ class URLSpider(scrapy.Spider):
     name = "url"
     start_urls = ['http://quotes.toscrape.com']
 
-    def __init__(self, max_depth=8, *args, **kwargs):
+    def __init__(self, max_depth=1, *args, **kwargs):
         super(URLSpider, self).__init__(*args, **kwargs)
         self.max_depth = int(max_depth)
 
@@ -20,17 +20,20 @@ class URLSpider(scrapy.Spider):
         os.makedirs(output_dir, exist_ok=True)
 
         # Sets the output filename to be the webpage's title
+        # TODO: Need unique names for webpages due to overwriting for duplicates.
+        # Or need to prune duplicate pages (ie page 1 + page 2)
         title = response.css('title::text')[0].extract().replace(" ","")
         filename = os.path.join(output_dir, f"{title}.html")
 
         # Write to file
         with open(filename, 'wb') as file:
             file.write(response.body)
+        self.log(f"Saved file {filename} (depth {depth})") # Log to show depth + filename
 
         # Break if depth is at or past max depth
         if depth >= self.max_depth:
             return
 
         # Otherwise get links and follow them, incrimenting depth
-        for link in response.css('a.attr(href)').getall():
+        for link in response.css('a::attr(href)').getall():
             yield response.follow(link, callback=self.parse, meta={'depth': depth+1})
