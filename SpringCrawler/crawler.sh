f1c1dcc896892fb94ea9955ea1c9756bc0a2f1d4
cathuyenphan <cat.phan9@gmail.com>
2025-05-05 12:26:19 -0700

    Fixed crawler.bat and crawler.sh

--- SpringCrawler/crawler.sh
+++ SpringCrawler/crawler.sh
@@ -8,6 +8,8 @@ fi
 
 SEED_FILE=$1
 NUM_PAGES=$2
+DEPTH=$3
+OUTPUT_DIR=$4
 
 if [ ! -d "venv" ]; then
   python3 -m venv venv
@@ -17,4 +19,16 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-scrapy crawl url -a seed_file=$SEED_FILE -a num_pages=$NUM_PAGES
+SCRAPY_CMND="scrapy crawl url -a seed_file=$SEED_FILE -a num_pages=$NUM_PAGES"
+
+if [ -n "$DEPTH" ]; then
+  SCRAPY_CMND="$SCRAPY_CMND -a max_depth=$DEPTH"
+fi
+
+if [ -n "$OUTPUT_DIR" ]; then
+  SCRAPY_CMND="$SCRAPY_CMND -a output_dir=$OUTPUT_DIR"
+fi
+
+eval $SCRAPY_CMND
+
+  
