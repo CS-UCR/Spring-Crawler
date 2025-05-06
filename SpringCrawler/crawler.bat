b8e9211e316be544aec804974c0972147dd26d68
cathuyenphan <cat.phan9@gmail.com>
2025-05-05 20:55:27 -0700

    fixed executables again

--- SpringCrawler/crawler.bat
+++ SpringCrawler/crawler.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
-if \"%~1\"==\"\" (
-    echo Usage: crawler.bat ^<seed-file^> ^<num-pages^> ^<depth>^ ^<output_dir>^
+if "%~1"=="" (
+    echo Usage: crawler.bat ^<seed-file^> ^<num-pages^> ^<depth^> ^<output_dir^>
     exit /b 1
 )
-if \"%~2\"==\"\" (
-    echo Usage: crawler.bat ^<seed-file^> ^<num-pages^> ^<depth>^ ^<output_dir>^
+if "%~2"=="" (
+    echo Usage: crawler.bat ^<seed-file^> ^<num-pages^> ^<depth^> ^<output_dir^>
     exit /b 1
 )
 
@@ -19,7 +19,7 @@ if not exist venv (
     python -m venv venv
 )
 
-call venv\\Scripts\\activate.bat
+call venv\Scripts\activate.bat
 pip install --upgrade pip
 pip install -r requirements.txt
 
@@ -33,4 +33,4 @@ if not "%OUTPUT_DIR%"=="" (
     set SCRAPY_CMND=!SCRAPY_CMND! -a output_dir=%OUTPUT_DIR%
 )
 
-cmd /c %SCRAPY_CMND% 
+%SCRAPY_CMND%
